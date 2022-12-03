0b1daeb91e3a0c29d41ba3c07993dc8260014cfa
romulomelendez <romulomelendez.97@gmail.com>
2022-12-03 10:35:04 -0300

    removing about route

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/views/Home.vue'
 import Chat from '@/views/Chat.vue'
-const About = { template: '<h1>About</h1>' }
 
 const routes = [
   { path: '/', name: 'home',component: Home },
   { path: '/chat', name: 'chat', component: Chat },
-  { path: '/about', name: 'about', component: About}
 ]
 
 export const router = createRouter({
